efaba45baddb849209590183382d306a9f223e99
guero10delgado <60528507+guero10delgado@users.noreply.github.com>
2022-06-09 17:09:17 -0500

    Create API.java

---
+++ Backend/API.java
@@ -0,0 +1,141 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.stream.Collectors;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author jorgedelgado
+ */
+@WebServlet(urlPatterns = {"/API"})
+public class API extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet API</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet API at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+        
+        //Metodo POST
+        
+        //CORS (Cross Origin Resource Sharing)
+        
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setCharacterEncoding("UTF-8");
+        response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+        response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+
+        /*
+        ConnectionDataBase conx = new ConnectionDataBase(
+            ConnectionDataBase.TipoBaseDatos.Postgresql, 
+            "localhost", 
+            "", 
+            "", 
+            "", 
+            ConnectionDataBase.AutoCommit.False
+        );
+        */
+        
+        try(PrintWriter out = response.getWriter()){
+            
+            try{
+                
+                String jsonBody = new BufferedReader(new InputStreamReader(request.getInputStream())).lines().collect(Collectors.joining("\n"));
+                
+                if(jsonBody == null || jsonBody.trim().length() == 0)
+                    throw new Exception("El JSON esta vacio");
+                
+                JSONObject jObj = new JSONObject(jsonBody);
+                
+                String Nombre = jObj.get("nombre").toString();
+                String Apellido = jObj.get("apellido").toString();
+                String Email = jObj.get("email").toString();
+                String Passw = jObj.get("passw").toString();
+                String Direccion = jObj.get("direccion").toString();
+                String Ciudad = jObj.get("ciudad").toString();
+                String Estado = jObj.get("estado").toString();
+                String CP = jObj.get("codigoPostal").toString();
+                
+                System.out.println(Nombre);
+                
+            }catch(Exception ex){
+                
+            }
+        }
+        
+        
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
